155bf575d8e0060223d1d09bfdf6e72bdda80f81
Ow1beast <Ow1beast@github.com>
2018-08-29 21:31:33 +0600

    Initial commit

---
+++ firstapp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstapp {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Beep();
+            Console.Write(a);
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.Read();
+        }
+    }
+}
